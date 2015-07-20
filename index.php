4dfbcd826c54aae5f01e920889c9a7df8d8110e6
wanfju19 <wanfju19@gmail.com>
2015-07-19 22:28:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 foreach ($_POST AS $key => $value) {
     
     if (preg_match("/image/", $key)){
-        file_put_contents($_SERVER["DOCUMENT_ROOT"] . "/groogr/test/photo1/" . $key . ".jpg", file_get_contents($value));
+        file_put_contents($_SERVER["DOCUMENT_ROOT"] . "/test/photo1/" . $key . ".jpg", file_get_contents($value));
          //echo $key;
    }
     
